e3fac148f3f5a25c18ba313d9b55b754f949ed52
ruthvik <ruthvikshruthvik@gmail.com>
2025-12-19 12:52:48 +0530

    changes through fetch and merge

--- myapp/repocheck.py
+++ myapp/repocheck.py
@@ -2,3 +2,5 @@ created just to push the file after manking changeses
 git branch
 git branch namebranch
 git checkout namebranch
+
+git merege origin/branch--- add the content that changed in github to local system file
