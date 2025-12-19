459a930fa161f5b75ae1e492e9cb18f5a0e6cb28
ruthvik <ruthvikshruthvik@gmail.com>
2025-12-19 13:11:35 +0530

    add new

--- myapp/repocheck.py
+++ myapp/repocheck.py
@@ -1,1 +1,8 @@
 created just to push the file after manking changeses
+git branch
+git branch namebranch
+git checkout namebranch
+
+
+git fetch --- downloads the changeses that are made in the github
+git merge
