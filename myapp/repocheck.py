21fd022ba2fb85e04638b21b5a1dbbb0183265b8
DarshanH05 <darshanhgowda005@gmail.com>
2025-12-19 13:21:55 +0530

    Update repocheck.py

--- myapp/repocheck.py
+++ myapp/repocheck.py
@@ -3,11 +3,3 @@ git branch
 git branch namebranch
 git checkout namebranch
 
-
-
-git fetch --- downloads the changeses that are made in the github
-git merge
-
-git fetch --- downloads the changeses that are made in the github
-git pull --- do both download and merge
-new
